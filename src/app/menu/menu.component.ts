d4cf221c46f9b1484ee7e7c61a294cb447e1abf7
muhammadhamx <muhammadhamzasajid6@gmail.com>
2024-09-06 21:44:01 +0500

    Addition of menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+
+  itemForm: FormGroup = new FormGroup({})
+  isAddItems: boolean = true
+constructor(private fb : FormBuilder){}
+
+  ngOnInit(): void {
+    this.initForm()
+  }
+  initForm() {
+    this.itemForm = this.fb.group({
+      name:['', Validators.required],
+      description:['', Validators.required],
+      picture:[''],
+      price:['', Validators.required],
+    })
+  }
+
+  submitForm() {
+    console.log(this.itemForm.value)
+  }
+
+
 
 }
